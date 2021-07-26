34f82c2f47a2aee0386d45051d7dac6059d9dbb2
Abhishekr99 <abhishekrajkumar1999@gmail.com>
2021-07-26 17:48:40 +0530

    added user,jwt modules

--- src/main/java/com/abhishek/stockchart/controller/CompanyController.java
+++ src/main/java/com/abhishek/stockchart/controller/CompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abhishek.stockchart.entity.CompExchMap;
@@ -34,6 +35,7 @@ import com.abhishek.stockchart.service.CompanyService;
 
 @CrossOrigin(origins="*")
 @RestController
+//@RequestMapping("/api/test")
 public class CompanyController 
 {
 
